b585189b2478ec591b3b5219a98dd4dc62da2941
Alvin-He <shinypeach36@gmail.com>
2022-02-23 22:06:10 -0800

    more on ws

--- ws.js
+++ ws.js
@@ -1,5 +1,25 @@
 // WebSocket API overwrite 
 
+// const old_WebSocket = WebSocket;
+
+class ws {
+    constructor(url, protocols) {
+        this.binaryType = "blob", "arraybuffer"; 
+        this.bufferedAmount; 
+        this.extensions; 
+        this.protocols; 
+        this.readyState; 
+        this.url; 
+    }
+    send(data) {
+        console.log(data)
+    }
+    close(code, reason) {
+        code = code ? code : 1000; 
+        console.log(code, reason)
+    }
+}
+
 function socket_send() {
 
 }
@@ -7,6 +27,10 @@ function socket_send() {
 function socket_close() {
 
 }
+// let sock = new WebSocket('wss://www.example.com/socketserver')
+// console.log(sock)
+
+
 
 let close = new CloseEvent('close', {
     wasClean: false, 
