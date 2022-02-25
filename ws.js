35b9f4b795c57d596a859e1946b64c6c626f34ad
Alvin-He <71620129+Alvin-He@users.noreply.github.com>
2022-02-24 16:06:09 -0800

    more ws ........

--- ws.js
+++ ws.js
@@ -1,22 +1,51 @@
 // WebSocket API overwrite 
 
-// const old_WebSocket = WebSocket;
+/**
+ * Notes:
+ * 
+ * 2 choices: 
+ *  1: inhert from WebSocket, change the constructor to point to our own end point,
+ *  2: change the entire class, point every thing towards service worker, and let it handle the traffic 
+ */
+
+const old_WebSocket = Object.assign({}, WebSocket);
+
+const preSets = {
+    binaryType: {
+        blob: 'blob', 
+        arraybuffer: 'arraybuffer'
+    },
+    readyState: {
+        CONNECTING: 0, 
+        OPEN: 1, 
+        CLOSING: 2, 
+        CLOSED: 3
+    }
+    // bufferedAmount: 0 : unsigned long
+    // extensions: aWebSocket.extensions : DOMString
+    // protocols: aWebSocket.protocol : DOMString
+    // url: aWebSocket.url : DOMString
+}
 
 class ws {
     constructor(url, protocols) {
-        this.binaryType = "blob", "arraybuffer"; 
-        this.bufferedAmount; 
-        this.extensions; 
-        this.protocols; 
-        this.readyState; 
-        this.url; 
+        this.sock = old_WebSocket(url, protocols)
+        return this
+        // this.binaryType = "blob", "arraybuffer"; 
+        // this.bufferedAmount = 0; 
+        // this.extensions = ''; 
+        // this.protocols = ''; 
+        // this.readyState = preSets.readyState.CONNECTING; 
+        // this.url = ''; 
     }
     send(data) {
-        console.log(data)
+        console.log(data); 
+        this.sock.send(data); 
     }
     close(code, reason) {
         code = code ? code : 1000; 
-        console.log(code, reason)
+        console.log(code, reason); 
+        this.sock.close(code, reason); 
     }
 }
 
@@ -39,5 +68,10 @@ let close = new CloseEvent('close', {
 }); 
 
 let error = new Event('error'); 
-let message = new Event('message'); 
+let message = new MessageEvent('messsage', {
+    data: '', 
+    origin: '', 
+    lastEventId: '', 
+    source: window, 
+})
 let open = new Event('open');
